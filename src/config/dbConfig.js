4df8640aaf4b9cab33591b8340fa6e5ce1aa53c8
G-Shiy <51141508+G-shiy@users.noreply.github.com>
2021-09-04 16:29:51 -0300

    Delete src directory

--- src/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConfig = "mongodb+srv://admin:admin@HelloWorld.nfevk.mongodb.net/annotations?retryWrites=true&w=majority";
-const connection = mongoose.connect(dbConfig, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-module.exports = connection;
